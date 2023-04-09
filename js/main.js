ab4f05fbf4303fa1038228109df08efa34a30b3b
mFaridun <musirmonovfaridun@gmail.com>
2023-04-09 18:32:17 +0500

    login section

--- js/main.js
+++ js/main.js
@@ -9,3 +9,26 @@ function hideMenu() {
     navLinks.style.right = "-200px";
 }
 // navbar togglemenu end
+
+
+const form = document.querySelector('.form'),
+      formName = document.querySelector('.name'),
+      warning = document.querySelectorAll('.warning'),
+      relation = document.querySelector('.relationship'),
+      username = document.querySelector('.username'),
+      password = document.querySelector('.password'),
+      pass = document.querySelector('.pass'),
+      regBtn = document.querySelector('.regBtn');
+
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        if (!formName.value) {
+          warning[0].textContent = "Iltimos formani to'ldiring";
+          formName.classList.add('form-warning');
+        }
+        else {
+          warning[0].textContent = '';
+          formName.classList.remove('form-warning');
+        }
+      })
